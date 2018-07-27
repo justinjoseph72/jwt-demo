a4540d1c829e2ae2609d1c73e7b9312e1c7485e0
Justin Joseph <justin.joseph72@outlook.com>
2018-07-27 15:32:46 +0100

    adding logout fetures

--- src/main/java/com/yoti/connections/api/security/handler/YotiConnectionLogoutHandler.java
+++ src/main/java/com/yoti/connections/api/security/handler/YotiConnectionLogoutHandler.java
@@ -2,6 +2,7 @@ package com.yoti.connections.api.security.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,5 +14,6 @@ public class YotiConnectionLogoutHandler implements LogoutHandler {
     @Override
     public void logout(final HttpServletRequest httpServletRequest, final HttpServletResponse httpServletResponse, final Authentication authentication) {
     log.info("the url is {}",httpServletRequest.getRequestURI());
+        SecurityContextHolder.clearContext();
     }
 }
